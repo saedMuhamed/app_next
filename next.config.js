7bf818dc9b8ad6fa9f83a6886e0d482990d490bb
saedMuhamed <saedxabo@gmail.com>
2023-09-05 21:59:26 +0300

    adding next config to apiUrl

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+env: {
+  API_URL: "https://app-next-zeta.vercel.app/";
+}
+const apiUrl = process.env.API_URL;
+const res = await fetch("http://localhost:4000/tickets", {
+  cache: "no-store",
+});

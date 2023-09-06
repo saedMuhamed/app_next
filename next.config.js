c9b511c3e3119485ba00f4e9bc74a5180caee37c
saedMuhamed <saedxabo@gmail.com>
2023-09-06 16:40:29 +0300

    removed api url

--- next.config.js
+++ next.config.js
@@ -2,11 +2,3 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
-
-env: {
-  API_URL: "https://app-next-zeta.vercel.app/";
-}
-const apiUrl = process.env.API_URL;
-const res = await fetch("http://localhost:4000/tickets", {
-  cache: "no-store",
-});
